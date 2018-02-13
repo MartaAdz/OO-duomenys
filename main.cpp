ff119dcb5502e0db30f322641ceaeb0bf259e79e
Marta <adzgausku@gmail.com>
2018-02-13 13:11:15 +0200

    PArasyta programos pradzia

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 
-using namespace std;
+using std::cout; using std::cin; using std::endl; using std::string;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout<<"Iveskite studento varda: ";
+    string vardas;
+    cin>>vardas;
+
+    int nDarbas[10]; //masyvas namu darbu pazymiais saugoti
+    for(int i=0; i<5; i++) //ivedame 5 pazymius
+    {
+        cout<<"Iveskite "<<i+1<<" pazymi: ";
+        cin>>nDarbas[i];
+    }
+
+    int nDarSuma=0;
+    for(int i=0; i<5; i++) nDarSuma+=nDarbas[i];
+
+    cout<<endl;
+    cout<<"Iveskite egzamino pazymi: ";
+    int egzaminas;
+    cin>>egzaminas;
+
+    double galBalas=0.4*(nDarSuma/5)+0.6*egzaminas;
+    cout.precision (3);
+    cout<<galBalas;
+
     return 0;
 }
