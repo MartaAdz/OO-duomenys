11bc630a9ea489a237d4766a21c941483582e2a3
Marta <adzgausku@gmail.com>
2018-04-14 21:46:20 +0300

    istrinti nereikalingi failai

--- mediana.h
+++
@@ -1,7 +0,0 @@
-#ifndef MEDIANA_H
-#define MEDIANA_H
-#include "studentas.h"
-#include <vector>
-    double mediana(std::vector<int>& M, int kiek, int c);
-//    double mediana (studentas &S);
-#endif
--- vidurkis.h
+++
@@ -1,6 +0,0 @@
-#ifndef VIDURKIS_H
-#define VIDURKIS_H
-#include "studentas.h"
-    double vidurkis(int NDsuma, int kiekis, int egzas);
-//    double vidurkis(studentas &S);
-#endif
