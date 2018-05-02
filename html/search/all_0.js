f033a417fd737270e6ea8ff7c656164ec6201013
Marta <adzgausku@gmail.com>
2018-05-03 00:55:41 +0300

    sukurtas doxygen

---
+++ html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['objektiškai_20orientuota_20duomenų_20apdorojimo_20programa',['Objektiškai orientuota Duomenų apdorojimo programa',['../md__c_1__users__marta__documents_objektinis_uzduotys__antra_uzduotis_clone__r_e_a_d_m_e.html',1,'']]]
+];
